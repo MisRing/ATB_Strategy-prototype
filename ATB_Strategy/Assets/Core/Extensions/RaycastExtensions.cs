81bf7abfcc39241d2ba2159d26428d693e6ae911
MisRing <39882750+MisRing@users.noreply.github.com>
2026-04-25 20:22:15 +0300

    Update RaycastExtensions.cs

--- ATB_Strategy/Assets/Core/Extensions/RaycastExtensions.cs
+++ ATB_Strategy/Assets/Core/Extensions/RaycastExtensions.cs
@@ -6,12 +6,6 @@ public static class RaycastExtensions
 {
     public static bool IsPointerOverUIObject()
     {
-        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
-
-        eventDataCurrentPosition.position = PlayerInputController.MouseScreenPosition;
-        List<RaycastResult> results = new List<RaycastResult>();
-
-        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
-        return results.Count > 0;
+        return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
     }
 }
